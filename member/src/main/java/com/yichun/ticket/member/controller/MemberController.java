60f00ba14ea6cab7dc86d2e9e63abde2788017a7
EmilyQian2001 <1044824871@qq.com>
2023-08-24 18:31:15 +0800

    incorporate validation

--- member/src/main/java/com/yichun/ticket/member/controller/MemberController.java
+++ member/src/main/java/com/yichun/ticket/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.yichun.ticket.common.resp.CommonResp;
 import com.yichun.ticket.member.req.MemberRegisterReq;
 import com.yichun.ticket.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +26,7 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public CommonResp<Long> register(MemberRegisterReq req){
+    public CommonResp<Long> register(@Valid MemberRegisterReq req){
         long register = memberService.register(req);
         return new CommonResp<>(register);
     }

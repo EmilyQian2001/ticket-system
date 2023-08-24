4d4a076677f89ecef39f8e1b3470520f96ed0972
EmilyQian2001 <1044824871@qq.com>
2023-08-24 17:38:03 +0800

    encapsulate request parameters and response

--- member/src/main/java/com/yichun/ticket/member/service/MemberService.java
+++ member/src/main/java/com/yichun/ticket/member/service/MemberService.java
@@ -1,8 +1,14 @@
 package com.yichun.ticket.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.yichun.ticket.member.domain.Member;
+import com.yichun.ticket.member.domain.MemberExample;
 import com.yichun.ticket.member.mapper.MemberMapper;
+import com.yichun.ticket.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -12,5 +18,22 @@ public class MemberService {
 
     public int count(){
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(MemberRegisterReq req){
+        String mobile = req.getMobile();
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        if (CollUtil.isNotEmpty(list)){
+//            return list.get(0).getId(); //已有的话直接给他登录了？
+            throw new RuntimeException("手机号已注册");
+        }
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }

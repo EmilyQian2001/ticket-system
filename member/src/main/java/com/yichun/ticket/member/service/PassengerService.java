58a842934108c9b3887c2ff63b19ddcfb98931c5
EmilyQian2001 <1044824871@qq.com>
2023-08-26 23:02:36 +0800

    add passenger apis

---
+++ member/src/main/java/com/yichun/ticket/member/service/PassengerService.java
@@ -0,0 +1,27 @@
+package com.yichun.ticket.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import com.yichun.ticket.common.util.SnowUtil;
+import com.yichun.ticket.member.domain.Passenger;
+import com.yichun.ticket.member.mapper.PassengerMapper;
+import com.yichun.ticket.member.req.PassengerSaveReq;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PassengerService {
+
+    @Resource
+    private PassengerMapper passengerMapper;
+
+    //新增乘客信息
+    public void save(PassengerSaveReq req){
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        passenger.setId(SnowUtil.getSnowflakeNextId());
+        passenger.setCreateTime(now);
+        passenger.setUpdateTime(now);
+        passengerMapper.insert(passenger);
+    }
+}

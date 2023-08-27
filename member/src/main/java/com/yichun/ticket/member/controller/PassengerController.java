8bff147f32f7cbb3ea5bdb8cbd3b9fd69b29a84e
EmilyQian2001 <1044824871@qq.com>
2023-08-27 17:10:30 +0800

    add member-passenger query list api

--- member/src/main/java/com/yichun/ticket/member/controller/PassengerController.java
+++ member/src/main/java/com/yichun/ticket/member/controller/PassengerController.java
@@ -1,14 +1,16 @@
 package com.yichun.ticket.member.controller;
 
+import com.yichun.ticket.common.context.LoginMemberContext;
 import com.yichun.ticket.common.resp.CommonResp;
+import com.yichun.ticket.member.req.PassengerQueryReq;
 import com.yichun.ticket.member.req.PassengerSaveReq;
+import com.yichun.ticket.member.resp.PassengerQueryResp;
 import com.yichun.ticket.member.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -21,6 +23,12 @@ public class PassengerController {
     public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req){
         passengerService.save(req);
         return new CommonResp<>();
+    }
+    @GetMapping("/query-list")
+    public CommonResp<List<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req){
+        req.setMemberId(LoginMemberContext.getId());
+        List<PassengerQueryResp> list = passengerService.queryList(req);
+        return new CommonResp<>(list);
     }
 
 }
--- member/src/main/java/com/yichun/ticket/member/service/PassengerService.java
+++ member/src/main/java/com/yichun/ticket/member/service/PassengerService.java
@@ -2,13 +2,19 @@ package com.yichun.ticket.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
 import com.yichun.ticket.common.context.LoginMemberContext;
 import com.yichun.ticket.common.util.SnowUtil;
 import com.yichun.ticket.member.domain.Passenger;
+import com.yichun.ticket.member.domain.PassengerExample;
 import com.yichun.ticket.member.mapper.PassengerMapper;
+import com.yichun.ticket.member.req.PassengerQueryReq;
 import com.yichun.ticket.member.req.PassengerSaveReq;
+import com.yichun.ticket.member.resp.PassengerQueryResp;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PassengerService {
@@ -25,5 +31,16 @@ public class PassengerService {
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
+    }
+
+    public List<PassengerQueryResp> queryList(PassengerQueryReq req){
+        PassengerExample passengerExample = new PassengerExample();
+        PassengerExample.Criteria criteria= passengerExample.createCriteria();
+        //必须把criteria放在外面，这样可以叠加多个条件
+        if(ObjectUtil.isNotNull(req.getMemberId())){
+            criteria.andMemberIdEqualTo(req.getMemberId());
+        }
+        List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
+        return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
     }
 }

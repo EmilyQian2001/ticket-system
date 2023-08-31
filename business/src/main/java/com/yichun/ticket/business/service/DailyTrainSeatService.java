e90240abc396cd8d2c772f525b9cd61c635c0320
EmilyQian2001 <1044824871@qq.com>
2023-08-31 15:42:20 +0800

    add daily train-station/carriage/seat

---
+++ business/src/main/java/com/yichun/ticket/business/service/DailyTrainSeatService.java
@@ -0,0 +1,74 @@
+package com.yichun.ticket.business.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.yichun.ticket.common.resp.PageResp;
+import com.yichun.ticket.common.util.SnowUtil;
+import com.yichun.ticket.business.domain.DailyTrainSeat;
+import com.yichun.ticket.business.domain.DailyTrainSeatExample;
+import com.yichun.ticket.business.mapper.DailyTrainSeatMapper;
+import com.yichun.ticket.business.req.DailyTrainSeatQueryReq;
+import com.yichun.ticket.business.req.DailyTrainSeatSaveReq;
+import com.yichun.ticket.business.resp.DailyTrainSeatQueryResp;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DailyTrainSeatService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainSeatService.class);
+
+    @Resource
+    private DailyTrainSeatMapper dailyTrainSeatMapper;
+
+    public void save(DailyTrainSeatSaveReq req) {
+        DateTime now = DateTime.now();
+        DailyTrainSeat dailyTrainSeat = BeanUtil.copyProperties(req, DailyTrainSeat.class);
+        if (ObjectUtil.isNull(dailyTrainSeat.getId())) {
+            dailyTrainSeat.setId(SnowUtil.getSnowflakeNextId());
+            dailyTrainSeat.setCreateTime(now);
+            dailyTrainSeat.setUpdateTime(now);
+            dailyTrainSeatMapper.insert(dailyTrainSeat);
+        } else {
+            dailyTrainSeat.setUpdateTime(now);
+            dailyTrainSeatMapper.updateByPrimaryKey(dailyTrainSeat);
+        }
+    }
+
+    public PageResp<DailyTrainSeatQueryResp> queryList(DailyTrainSeatQueryReq req) {
+        DailyTrainSeatExample dailyTrainSeatExample = new DailyTrainSeatExample();
+        dailyTrainSeatExample.setOrderByClause("train_code asc, carriage_index asc, carriage_seat_index asc");
+        DailyTrainSeatExample.Criteria criteria = dailyTrainSeatExample.createCriteria();
+
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<DailyTrainSeat> dailyTrainSeatList = dailyTrainSeatMapper.selectByExample(dailyTrainSeatExample);
+
+        if(ObjectUtil.isNotEmpty(req.getTrainCode())){
+            criteria.andTrainCodeEqualTo(req.getTrainCode());
+        }
+
+        PageInfo<DailyTrainSeat> pageInfo = new PageInfo<>(dailyTrainSeatList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        List<DailyTrainSeatQueryResp> list = BeanUtil.copyToList(dailyTrainSeatList, DailyTrainSeatQueryResp.class);
+
+        PageResp<DailyTrainSeatQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+
+    public void delete(Long id) {
+        dailyTrainSeatMapper.deleteByPrimaryKey(id);
+    }
+}
